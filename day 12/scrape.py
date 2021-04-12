60f94b2a9575b3e46045ce26f58f7d1526e6d231
Yakov <kaktyzz-imba@mail.ru>
2021-04-12 11:56:28 +0300

    123

--- day 12/scrape.py
+++ day 12/scrape.py
@@ -1,13 +1,15 @@
+import os
+import sys
 import requests
 import datetime
 from requests_html import HTML
 import pandas as pd
 
+BASE_DIR = os.path.dirname(__file__)
 
-now = datetime.datetime.now()
-year = now.year
 
-url = 'https://www.boxofficemojo.com/year/world/'
+
+
 
 def url_to_txt(url, filename='world.html', save=False):
     r = requests.get(url)
@@ -17,20 +19,19 @@ def url_to_txt(url, filename='world.html', save=False):
             with open(f"world-{year}.html", 'w',encoding='utf-8')as f:
                 f.write(html_text)
         return html_text
-    return ""
+    return None
 
-html_text = url_to_txt(url)
-
-r_text = HTML(html=html_text)
-
-table_class = ".imdb-scroll-table"
-
-r_table = r_text.find(table_class)
-
-table_data = []
-header_names = []
-if len(r_table) == 1:
-    
+def parse_and_extract(url, name='2020'):
+    html_text = url_to_txt(url)
+    if html_text == None:
+        return False
+    r_text = HTML(html=html_text)
+    table_class = ".imdb-scroll-table"
+    r_table = r_text.find(table_class)
+    table_data = []
+    header_names = []
+    if len(r_table) == :
+        return False
     parsed_table = r_table[0]
     rows = parsed_table.find("tr")
     header_row = rows[0]
@@ -42,12 +43,45 @@ if len(r_table) == 1:
         cols = row.find("td")
         row_data = []
         for i, col in enumerate(cols):
-           # print(i, col.text, '\n\n')
+        # print(i, col.text, '\n\n')
             row_data.append(col.text)
         table_data.append(row_data)
 
-print(table_data)
-print(header_names)
+    path = os.path.join(BASE_DIR, 'data')
+    os.makedirs(path, exist_ok=True)
+    filepath = os.path.join('data',f'{name}.csv')
 
-df = pd.DataFrame(table_data, columns=header_names)
-df.to_csv('movies.csv', index=False)
+    df = pd.DataFrame(table_data, columns=header_names)
+    df.to_csv(filepath, index=False)
+    return True
+
+
+def run(start_year=None, years_ago=0):
+    if start_year == None:
+        now = datetime.datetime.now()
+        start_year = now.year
+    assert isinstance(start_year, int)
+    assert len(f"{start_year}") == 4
+    assert isinstance(years_ago, int)
+
+    for i in range(0, years_ago+1):
+        url = f'https://www.boxofficemojo.com/year/world/{start_year}'
+        finished = parse_and_extract(url, name=start_year)
+        if finished:
+            print(f"Finished {start_year}")
+        else:
+            print(f"{start_year} not finished")
+        start_year -= 1
+
+
+if __name__ == "__main__":
+    
+    try:
+        start = int(sys.argv[1])
+    except:
+        start = None
+    try:
+        count = int(sys.argv[2])
+    except:
+        count = 0
+    run(start_year=start, years_ago=count)
